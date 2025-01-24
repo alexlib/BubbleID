0689790db286389a09e486d6d482f6583c4aa1af
Christy Dunlap <christydunlap26@gmail.com>
2025-01-24 11:57:19 -0600

    update

--- source/conf.py
+++ source/conf.py
@@ -27,4 +27,5 @@ extensions = [
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
+html_title = 'BubbleID'
 html_static_path = []
